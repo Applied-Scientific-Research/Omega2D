4d6856dac289928372826fb591e2b237992f8401
Mark Stock <markjstock@gmail.com>
2019-01-19 10:01:35 -0500

    Adding boundary features to json reader

--- src/JsonHelper.cpp
+++ src/JsonHelper.cpp
@@ -71,11 +71,11 @@ void read_json (Simulation& sim,
 
   // read the flow features, if any
   if (j.count("flowstructures") == 1) {
-    std::vector<json> ff_json = j["flowstructures"];
+    const std::vector<json> ff_json = j["flowstructures"];
     // iterate through vector of flow features
     for (auto const& ff: ff_json) {
       //if (ff.count("type") == 1) {
-      std::string ftype = ff["type"];
+      const std::string ftype = ff["type"];
       if (ftype == "single particle") {
         std::cout << "  found single particle" << std::endl;
         const float str = ff["strength"];
@@ -100,6 +100,50 @@ void read_json (Simulation& sim,
         const float str = ff["strength"];
         const std::vector<float> c = ff["center"];
         ffeatures.emplace_back(std::make_unique<ParticleEmitter>(c[0], c[1], str));
+      }
+    }
+  }
+
+  // read the boundary features, if any
+  if (j.count("bodies") == 1) {
+    std::vector<json> bdy_json = j["bodies"];
+    // iterate through vector of bodies and generate boundary features
+    for (auto const& bdy: bdy_json) {
+      // make a new Body (later) and attach these geometries to it
+      // see if there are meshes
+      if (bdy.count("meshes") == 1) {
+        std::vector<json> bf_json = bdy["meshes"];
+        // iterate through vector of meshes, all on this body
+        for (auto const& bf: bf_json) {
+          const std::string ftype = bf["geometry"];
+          const std::vector<float> tr = bf["translation"];
+          const float rot = bf["rotation"];
+          // load scale into an object, because it can be a number or an array of numbers
+          const json sc = bf["scale"];
+          // split on geometry name
+          if (ftype == "circle") {
+            std::cout << "  found solid circle" << std::endl;
+            if (sc.is_number()) {
+              const float scale = bf["scale"];
+              bfeatures.emplace_back(std::make_unique<SolidCircle>(tr[0], tr[1], scale));
+            }
+          } else if (ftype == "oval") {
+            std::cout << "  found solid oval" << std::endl;
+            if (sc.is_array()) {
+              const std::vector<float> scale = bf["scale"];
+              bfeatures.emplace_back(std::make_unique<SolidOval>(tr[0], tr[1], scale[0], scale[1], rot));
+            }
+          } else if (ftype == "square") {
+            std::cout << "  found solid square" << std::endl;
+            if (sc.is_number()) {
+              const float scale = bf["scale"];
+              bfeatures.emplace_back(std::make_unique<SolidSquare>(tr[0], tr[1], scale, rot));
+            }
+          } else {
+            // it's not a predefined geometry, but a file
+            std::cout << "  found geometry file - but not loading it" << std::endl;
+          }
+        }
       }
     }
   }
