4a4991f54b5f2545215f5eef5290511b66a8b3f2
Mark Stock <markjstock@gmail.com>
2019-06-25 17:30:11 -0400

    Small change to allow Release builds on Windows - I hope this works in Linux

--- src/tinyexpr/tinyexpr.c
+++ src/tinyexpr/tinyexpr.c
@@ -149,6 +149,9 @@ static double ncr(double n, double r) {
 }
 static double npr(double n, double r) {return ncr(n, r) * fac(r);}
 
+#pragma function (ceil)
+#pragma function (floor)
+
 static const te_variable functions[] = {
     /* must be in alphabetical order */
     {"abs", fabs,     TE_FUNCTION1 | TE_FLAG_PURE, 0},
