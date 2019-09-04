8068710ba3613368421fd1befd890a170a199475
Mark Stock <markjstock@gmail.com>
2019-09-03 20:01:42 -0400

    Widened available vort density draw scale

--- src/main_gui.cpp
+++ src/main_gui.cpp
@@ -1327,7 +1327,7 @@ int main(int argc, char const *argv[]) {
       ImGui::ColorEdit3("feature color",        rparams.default_color);
       ImGui::ColorEdit3("background color",     rparams.clear_color);
       //ImGui::Checkbox("show origin", &show_origin);
-      ImGui::SliderFloat("vorticity density", &(rparams.circ_density), 0.1f, 1.0f, "%.2f");
+      ImGui::SliderFloat("vorticity density", &(rparams.circ_density), 0.01f, 10.0f, "%.2f", 2.0f);
 
       if (ImGui::Button("Recenter")) {
         // put everything back to center
