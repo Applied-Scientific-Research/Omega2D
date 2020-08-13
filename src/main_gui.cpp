653184789b6ad4c516bf373f25eeb4dafb147e4a
Blake Hillier <blakehillier@mac.com>
2020-08-12 17:11:10 -0700

    main_batch now creates features using element packets, and fixed error in main_gui

--- src/main_gui.cpp
+++ src/main_gui.cpp
@@ -316,13 +316,20 @@ int main(int argc, char const *argv[]) {
 
       if (sim_err_msg.empty()) {
         // the last simulation step was fine, OK to continue
-
         // generate new particles from emitters
-        for (auto const& ff : ffeatures) {
-          if (ff->is_enabled()) sim.add_elements( ff->step_elements(sim.get_ips()) );
+        for (auto const& ff: ffeatures) {
+          if (ff->is_enabled()) {
+            ElementPacket<float> newpacket = ff->step_elements(sim.get_ips());
+            // echo any errors
+             sim.add_elements( newpacket, active, lagrangian, ff->get_body() );
+          }
         }
-        for (auto const& mf : mfeatures) {
-          if (mf->is_enabled()) sim.add_elements( mf->step_elements(rparams.tracer_scale*sim.get_ips()), true );
+
+        for (auto const& mf: mfeatures) {
+          //if (mf->is_enabled()) sim.add_fldpts( mf->init_particles(rparams.tracer_scale*sim.get_ips()), mf->moves() );
+          if (mf->is_enabled()) {
+            const move_t newMoveType = (mf->get_is_lagrangian() ? lagrangian : fixed);
+            sim.add_elements( mf->step_elements(rparams.tracer_scale*sim.get_ips()), inert, newMoveType, mf->get_body() );
         }
 
         // begin a new dynamic step: convection and diffusion
