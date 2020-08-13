2bbcb3bfacd70af410c3e9cccdbd487f55e51f44
Mark Stock <markjstock@gmail.com>
2020-08-12 23:42:01 -0400

    Cleaned up add_elements calls from main

--- src/main_gui.cpp
+++ src/main_gui.cpp
@@ -211,10 +211,9 @@ int main(int argc, char const *argv[]) {
         //if (ff->is_enabled()) sim.add_particles( ff->init_particles(sim.get_ips()) );
         if (ff->is_enabled()) {
           ElementPacket<float> newpacket = ff->init_elements(sim.get_ips());
-          // echo any errors
-          /*if (good)*/ sim.add_elements( newpacket, active, lagrangian, ff->get_body() );
+          // check for errors here?
+          sim.add_elements( newpacket, active, lagrangian, ff->get_body() );
         }
-        //if (ff->is_enabled()) sim.add_elements( ff->init_elements(sim.get_ips()), active, lagrangian, ff->get_body() );
       }
 
       // initialize solid objects
@@ -222,8 +221,8 @@ int main(int argc, char const *argv[]) {
         //if (bf->is_enabled()) sim.add_boundary( bf->get_body(), bf->init_elements(sim.get_ips()) );
         if (bf->is_enabled()) {
           ElementPacket<float> newpacket = bf->init_elements(sim.get_ips());
-          const move_t newmovetype = (bf->get_body() ? bodybound : fixed);
-          sim.add_elements(newpacket, reactive, newmovetype, bf->get_body() );
+          const move_t newMoveType = (bf->get_body() ? bodybound : fixed);
+          sim.add_elements(newpacket, reactive, newMoveType, bf->get_body() );
         }
       }
 
@@ -231,8 +230,9 @@ int main(int argc, char const *argv[]) {
       for (auto const& mf: mfeatures) {
         //if (mf->is_enabled()) sim.add_fldpts( mf->init_particles(rparams.tracer_scale*sim.get_ips()), mf->moves() );
         if (mf->is_enabled()) {
+          ElementPacket<float> newpacket = mf->init_elements(rparams.tracer_scale*sim.get_ips());
           const move_t newMoveType = (mf->get_is_lagrangian() ? lagrangian : fixed);
-          sim.add_elements( mf->init_elements(rparams.tracer_scale*sim.get_ips()), inert, newMoveType, mf->get_body() );
+          sim.add_elements(newpacket, inert, newMoveType, mf->get_body() );
         }
       }
 
